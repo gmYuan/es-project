97bc68437faa07d35e3913e0f52f020e4285978b
ygm <ygm121351@163.com>
2023-10-14 16:00:59 +0800

    feat: 初始化项目

---
+++ 01-js基础/01-代码后备/5.js/18.js
@@ -0,0 +1,13 @@
+console.log('1');
+
+setTimeout(function () {
+    console.log('t1');
+}, 0);
+
+Promise.resolve().then(function () {
+    console.log('p1');
+}).then(function () {
+    console.log('p2');
+});
+
+console.log('2');

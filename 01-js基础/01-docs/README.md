be073a6ab42c5ac6a9eb9fb6bece4653fe57f1e5
ygm <ygm121351@163.com>
2023-10-29 23:05:39 +0800

    add

--- 01-js基础/01-docs/README.md
+++ 01-js基础/01-docs/README.md
@@ -1,11 +1,3 @@
-
-
-
-
-
-
-
-
 ## 参考文档
 
 [01-JS基础](http://www.zhufengpeixun.com/strong/html/94.1.JavaScript.html)
--- 01-js基础/100-代码后备/1.js/18.js
+++
@@ -1,8 +0,0 @@
-var funs = [];
-for (let i = 0; i < 6; i++) {
-    funs.push(function () {
-        console.log(i);
-    });
-}
-debugger
-funs[3]();//3

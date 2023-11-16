edaa73785fd6cdc810192ecceb7097183958fe0e
ygm <ygm121351@163.com>
2023-11-16 22:01:37 +0800

    feat: add new

--- 03-TS/02-珠峰ts1/2.ts
+++ 03-TS/02-珠峰ts1/2.ts
@@ -1,58 +1,53 @@
-
-
-function hello(name:string):void{
-  console.log('hello',name);
+function hello(name: string): void {
+  console.log("hello", name);
 }
-hello('zhufeng');
+hello("zhufeng");
 
-type GetName = (firstName:string,lastName:string)=>string;
-let getName: GetName = function (firstName: string, lastName: string): string{
-    return firstName+lastName;
+type GetName = (firstName: string, lastName: string) => string;
+let getName: GetName = function (firstName: string, lastName: string): string {
+  return firstName + lastName;
+};
+
+function print(name: string, age?: number): void {
+  console.log(name, age);
 }
+print("zhufeng", 11);
 
-function print(name:string,age?:number):void{
-  console.log(name,age);
-  
+function ajax(url: string, method: string = "GET") {
+  console.log(url, method);
 }
-print('zhufeng',11);
+ajax("/");
 
-function ajax(url:string,method:string='GET'){
-  console.log(url,method);
+function sum(...numbers: number[]) {
+  return numbers.reduce((val, item) => val + item, 0);
 }
-ajax('/');
+console.log(sum(1, 2, 3));
 
-function sum(...numbers:number[]){
-    return numbers.reduce((val,item)=>val+item,0)
-}
-console.log(sum(1,2,3));
-
-//函数的重载 
-let obj:any = {};
+//函数的重载
+let obj: any = {};
 //function attr():void
 /**
  * 如果传的val是一个字符串赋给obj.name
  *                 数字,赋给obj.age
- * @param val 
+ * @param val
  */
-function attr(val: string): void
-function attr(val: number): void
-function attr(val:any):void{
-    if(typeof val === 'string'){
-        obj.name = val;
-    } else if (typeof val === 'number'){
-        obj.age = val;
-    }
+function attr(val: string): void;
+function attr(val: number): void;
+function attr(val: any): void {
+  if (typeof val === "string") {
+    obj.name = val;
+  } else if (typeof val === "number") {
+    obj.age = val;
+  }
 }
-attr('zhufeng');
+attr("zhufeng");
 attr(10);
 //attr(true);
-function add(a: string,b:string): void
-function add(a: number,b:number): void
-function add(a: string|number,b:string|number): void {
-   
-}
-add('a','b');
-add(1,2);
+function add(a: string, b: string): void;
+function add(a: number, b: number): void;
+function add(a: string | number, b: string | number): void {}
+add("a", "b");
+add(1, 2);
 //add(1,'b');
 
 //非常有名的案例 compose

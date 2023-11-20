72ec13ff96feaebc9c9c3a749e70af3f7ad252fa
ygm <ygm121351@163.com>
2023-11-20 23:25:16 +0800

    add

--- 03-TS/02-珠峰ts1/14-泛型约束与number.ts
+++ 03-TS/02-珠峰ts1/14-泛型约束与number.ts
@@ -1,5 +1,4 @@
-
-export {}
+export {};
 class Father {
   father: string;
 }
@@ -9,20 +8,19 @@ class Child1 extends Father {
 class Child2 extends Father {
   child1: string;
 }
-interface Calculate{
-  <T extends (string | number)>(a:T,b:T):void
+
+interface Calculate {
+  <T extends string | number>(a: T, b: T): void;
 }
-//'number' is assignable to the constraint of type 'T', 
+
+//'number' is assignable to the constraint of type 'T',
 //but 'T' could be instantiated with a different subtype of constraint 'number'.
 //不能说只能多少 只能适用于接口或对象属性上来说才是只多不少
 //严格来说就是子类型
-let sum: Calculate = function <T extends (string | number)>(a: T, b: T): void {
+let sum: Calculate = function <T extends string | number>(a: T, b: T): void {};
 
-};
 /**
  * string 是(string | number)的子类型
  * (string | number)是(string|number|boolean)的子类型
  */
 //sum<(string|number|boolean)>(null, null);
-
-
